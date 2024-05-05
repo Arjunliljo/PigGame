f2b8ee514aea707fbefaff984dbdfa59530ad86a
Arjun cv <arjun7180@gmail.com>
2024-05-05 23:45:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-'use strict';
 
-// initializing html elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 let diceEl = document.querySelector('.dice');
